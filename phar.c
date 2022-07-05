e367266fa762cef3ef3b5b2d70327ea03e161bfb
JuliusDgenius <ibejulius1@gmail.com>
2022-07-05 03:37:51 +0100

    phar.c

--- phar.c
+++ phar.c
@@ -1,1 +1,20 @@
 #include "monty.h"
+
+/**
+ * _pchar - prints character at the top of stack
+ * @stack: A pointer to linked list stack
+ * @line_number: number of line opcode occurs on
+ *
+ */
+void _pint(stack_t **stack, unsigned char line_number)
+{
+	stack_t *runner;
+
+	runner = *stack;
+	if (runner == NULL)
+	{
+		fprintf(stderr, "L%d: can't print, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+		printf("%d\n", runner->n);
+}
